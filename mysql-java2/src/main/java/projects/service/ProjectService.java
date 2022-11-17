b475fe126fd315b778dc2e292fd0ae78707f1db0
Andres Mejia <andresmejia7@outlook.com>
2022-11-17 00:24:21 -0800

    Week10

--- mysql-java2/src/main/java/projects/service/ProjectService.java
+++ mysql-java2/src/main/java/projects/service/ProjectService.java
@@ -1,13 +1,31 @@
 package projects.service;
 
+import java.util.List;
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import java.util.NoSuchElementException;
+
 
 public class ProjectService {
   private ProjectDao projectDao = new ProjectDao();
 
   public Project addProject(Project project) {
     return projectDao.insertProject(project);
+  }
+
+  public List<Project> fetchAllProjects() {
+    return projectDao.fetchAllProjects();
+  }
+
+  /**
+   * 
+   * @param projectId The project ID.
+   * @return A Project object if successful.
+   * @throws NoSuchElementException Thrown if the project with the given ID does not exist.
+   */
+  public Project fetchProjectById(Integer projectId) {
+    return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
+        "Project with project ID = " + projectId + " does not exist."));
   }
 
 }
